a0291efd16949fdf994f4cbb3ffc24af3a0881b0
Yasin <mad_rock@hotmail.com>
2018-02-03 12:33:01 +0300

    proje oluşturuldu harita eklendi.

---
+++ Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Bulut Kapalılık Haritası</title>
+
+    <link rel="stylesheet" href="./css/site.css" />
+    <link rel="stylesheet" href="./css/leaflet.css" />
+    <script src="./js/leaflet.js"></script>
+    <script src="js/dataModel.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+
+</head>
+<body onload="yukle()">
+    <form id="form1" runat="server">
+        <div id="map"></div>
+
+        
+        <script type="text/javascript">
+
+            var map = L.map('map').setView([39.0, 35.5], 4);
+            // add an OpenStreetMap tile layer
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://www.mgm.gov.tr">Meteoroloji Genel Müdürlüğü</a>'
+            }).addTo(map);
+
+            function yukle() {
+                console.log(title);
+                console.log(value);
+                console.log(title[dataModel.istNo] + " " + value[0][dataModel.istNo]);
+            }
+
+
+            
+        </script>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        form1.InnerHtml+="<script>" + FileToArray(new DateTime(2018, 01, 25, 14, 0, 0)) + "</script>";
+    }
+
+    string FileToArray(DateTime dt)
+    {
+        string[] title, value;
+
+        string output = "", path = "\\data\\";
+        string fileName = dt.ToString("yyyyMMddHH") + ".csv";
+        string theFile = path + fileName;
+        StreamReader sr = File.OpenText(theFile);
+        title = sr.ReadLine().Split(';');
+        output += "title=[";
+        for (int i = 0; i < title.Length; i++)
+            output += "'"+title[i] + "',";
+        output = output.TrimEnd(',');
+        output += "];";
+
+        String line;
+        int j;
+        output += "value=[";
+        for (line = sr.ReadLine(), j = 0; line != null; line = sr.ReadLine(), j++) //while ((line = sr.ReadLine()) != null)
+        {
+            value = line.Split(';');
+            output += "[";
+            for (int i = 0; i < title.Length; i++)
+            {
+                output += "'" + value[i] + "',";   //output += "value[" + j + "][" + i + "] = " + value[i];
+            }
+            output += "],";
+        }
+
+        output = output.TrimEnd(',');
+        output += "]";
+
+        return output;
+    }
+}
