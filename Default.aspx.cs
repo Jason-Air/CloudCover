d87d708a40ef198bd7b0c217e82949a530c4b202
Yasin <mad_rock@hotmail.com>
2018-02-07 16:51:11 +0300

    kod sadeleştirildi

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -22,31 +22,23 @@ public partial class _Default : System.Web.UI.Page
         string fileName = dt.ToString("yyyyMMddHH") + ".csv";
         string theFile = path + fileName;
         string[] content;
-        //int index = 0, len = 0; ;
-        //StreamReader sr = File.OpenText(theFile);
-        //title = sr.ReadLine().Split(';');
+
         content = File.ReadAllLines(theFile);
-        //len = content.IndexOf('\n');
+        
         title = content[0].Split(';');
         output += "title=[";
         for (int i = 0; i < title.Length; i++)
             output += "'" + title[i] + "',";
         output = output.TrimEnd(',');
         output += "];";
-        //content = content.Substring(len);
-
         output += "value=[";
-        //for (line = sr.ReadLine(), j = 0; line != null; line = sr.ReadLine(), j++) //while ((line = sr.ReadLine()) != null)
-        
-        //string[] line = content.Split(('\n'));
-        //for (index += len, len = content.IndexOf('\n', index), line = content.Substring(index, len); line != null; index += len, len = content.IndexOf('\n', index), line = content.Substring(index, len))
-        for (int i=1;i<content.Length;i++) //foreach(string l in line)
+        for (int i=1;i<content.Length;i++) 
         {
             value = content[i].Split(';');
             output += "[";
-            foreach (var v in value) //for (int i = 0; i < title.Length; i++)
+            foreach (var v in value) 
             {
-                output += "'" + v + "',";   //output += "value[" + j + "][" + i + "] = " + value[i];
+                output += "'" + v + "',";   
             }
             output = output.TrimEnd(',');
             output += "],";
